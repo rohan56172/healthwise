a4a07daec7fbfd3883f04f886b66817ffff4fa8c
P. Vaibhav Goud <pv22btb0a43@student.nitw.ac.in>
2026-01-18 15:51:30 +0530

    cors connection enabled

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,10 @@ const app = express();
 const PORT=process.env.PORT||5000;
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true                
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
