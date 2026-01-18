29208d36adec9bd1da0e22cd5220d07669db3c99
rohan56172 <rohansocial345@gmail.com>
2026-01-18 18:12:58 +0530

    first step to modify the frontend

--- frontend/lib/apiaxios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:7000/api", // Matches your backend PORT
-  withCredentials: true, // IMPORTANT: Allows cookies to be sent/received
-});
-
-export default api;
